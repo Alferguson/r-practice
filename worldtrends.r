88b23cf86fa960752e096d1e8ef23283daa4f959
alferguson <alferguson@berkeley.edu>
2018-04-03 10:33:41 -0700

    starting homework

---
+++ worldtrends.r
@@ -0,0 +1,18 @@
+# The World Bank was very impressed with your deliverables on the previous
+# assignment and they have a new project for you.
+# You are required to produce a scatterplot depicting Life Expectancy (y-axis) and
+# Fertility Rate (x-axis) statistics by Country.
+# The scatterplot needs to also be categorised by Countries’ Regions.
+
+# You have been supplied with data for 2 years: 1960 and 2013 and you are required
+# to produce a visualisation for each of these years.
+# Some data has been provided in a csv file, some - in R vectors. The csv file contains
+# combined data for both years. All data manipulations have to be performed in R (not
+# in excel) because this project may be audited at a later stage.
+# You have also been requested to provide insights into how the two periods compare.
+
+# After you import the csv file into R the first step is to split
+# the data frame into two:
+# data1960 and data2013
+# In order to achieve this use data frame filtering we
+# discussed in this section.
